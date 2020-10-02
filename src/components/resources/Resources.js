847ab2299dd31a437d78a18ed3d8928c0a22f135
Boyan Kmetov <bkmetoff@gmail.com>
2020-10-02 11:24:37 +0200

    feat: basic file structire & theme variables

---
+++ src/components/resources/Resources.js
@@ -0,0 +1,28 @@
+export const COPY = {
+	people: [
+		{
+			Natalia: {
+				fullName: '',
+				bio: '',
+			},
+		},
+	],
+	pages: ['About', ' Musicians', 'Listen', 'Agenda', 'Gallery', 'Contact'],
+	bio: {
+		ensemble: '',
+		patron: '',
+	},
+	mission: '',
+	links: [],
+}
+
+export const MEDIA = {
+	// urls
+	cover: '',
+	photos: [
+		{
+			title: '',
+			title2: '',
+		},
+	],
+}
---
+++ src/components/theme/Theme.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Theme = {
+	borderRadius: '3px',
+	shadow: '0 2px 5px rgba(0, 0, 0, 0.2)',
+	colors: {
+		primary: '#1e2548',
+		light: '#fff',
+		hover: '',
+	},
+}
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+  margin: 0;
+  font-weight: 400;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+*,
+*::after,
+*::before {
+  padding: 0;
+  margin: 0;
+  box-sizing: inherit;
+  /* overflow-x: hidden; */
+  -webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-rendering: optimizeLegibility;
+	-webkit-tap-highlight-color: transparent;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  :hover {
+    cursor: pointer;
+  }
+}
+
+::placeholder {
+  text-align: center;
+}
+
+`
