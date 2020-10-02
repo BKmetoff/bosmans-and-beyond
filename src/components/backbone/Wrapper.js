d06dfdccb039023f3a5a147d7ca24487d504f824
Boyan Kmetov <bkmetoff@gmail.com>
2020-10-02 15:46:56 +0200

    feat: react routes

---
+++ src/components/backbone/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const MainWrapper = styled.div`
+	top: 60px;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`
+export default MainWrapper
