9e5add620dea40599ad0bbd2c4152bc5ab41111a
Boyan Kmetov <bkmetoff@gmail.com>
2020-10-17 13:32:55 +0200

    feat: add error page & route

---
+++ src/components/NotFound.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styled from 'styled-components'
+
+import { SectionWrapper, MotionWrapper } from './backbone/Wrapper'
+import Button from './backbone/Button'
+import { Title } from './backbone/Text'
+
+const NotFoundTitle = styled(Title)`
+	width: fit-content;
+	border: none;
+	margin: 0;
+	display: flex;
+	text-align: center;
+`
+
+export default function NotFound() {
+	return (
+		<MotionWrapper>
+			<SectionWrapper>
+				<NotFoundTitle>
+					Oops, we couldn't find what you were looking for!
+				</NotFoundTitle>
+				<Button type='primary'>
+					<Link to='/'>Go home </Link>
+				</Button>
+			</SectionWrapper>
+		</MotionWrapper>
+	)
+}
