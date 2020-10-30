834389cba4f569fb476067fdc69af41e04801a88
Boyan Kmetov <bkmetoff@gmail.com>
2020-10-30 15:00:38 +0100

    fix: WIP: contact & form - change action tag to method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-import { SectionWrapper } from './backbone/Wrapper'
+import { SectionWrapper, MotionWrapper } from './backbone/Wrapper'
 import Form from './backbone/Form'
 
 export default function Contact() {
 	return (
-		<SectionWrapper>
-			<Form />
-		</SectionWrapper>
+		<MotionWrapper>
+			<SectionWrapper>
+				<Form />
+			</SectionWrapper>
+		</MotionWrapper>
 	)
 }
