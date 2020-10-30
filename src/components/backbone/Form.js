83b7be40d81bd5f50387401834d9a9456d5b0428
Boyan Kmetov <bkmetoff@gmail.com>
2020-10-30 16:29:10 +0100

    fix: WIP: contact page & form - redirect to home. clean-up

--- src/components/backbone/Form.js
+++ src/components/backbone/Form.js
@@ -16,33 +16,12 @@ const BaseForm = styled.form`
 export default function Form() {
 	console.log('form')
 	return (
-		<BaseForm action='/contact' name='contact' method='post'>
+		<BaseForm action='/' name='contact' method='post'>
 			<input type='hidden' name='form-name' value='contact' />
 			<BaseInput name='email' type='email' placeholder='email' />
 			<BaseInput name='name' type='text' placeholder='name' />
 			<BaseInput name='message' type='text' placeholder='message' />
 			<Button type='submit'>Submit</Button>
 		</BaseForm>
-		// <form name='contact' method='POST' data-netlify='true' action='/success'>
-		// 	<p>
-		// 		<label>
-		// 			Your Name: <input type='text' name='name' />
-		// 		</label>
-		// 	</p>
-		// 	<p>
-		// 		<label>
-		// 			Your Email: <input type='email' name='email' />
-		// 		</label>
-		// 	</p>
-		// 	<p>
-		// 		<label>
-		// 			Message: <textarea name='message'></textarea>
-		// 		</label>
-		// 	</p>
-		// 	<p>
-		// 		<button type='submit'>Send</button>
-		// 	</p>
-		// 	<div data-netlify-recaptcha='true' />
-		// </form>
 	)
 }
