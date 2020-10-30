bce1d83c95eba1c7c2e5f26eac0e519c57eca878
Boyan Kmetov <bkmetoff@gmail.com>
2020-10-30 14:17:01 +0100

    feat: WIP: contact page & form - trying out an example html form

--- src/components/backbone/Form.js
+++ src/components/backbone/Form.js
@@ -1,26 +1,47 @@
 import React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-import Button from './Button'
+// import Button from './Button'
 
-const BaseInput = styled.input`
-	width: 200px;
-	height: 40px;
-`
+// const BaseInput = styled.input`
+// 	width: 200px;
+// 	height: 40px;
+// `
 
-const BaseForm = styled.form`
-	display: flex;
-	flex-direction: column;
-`
+// const BaseForm = styled.form`
+// 	display: flex;
+// 	flex-direction: column;
+// `
 
 export default function Form() {
 	return (
-		<BaseForm data-netlify='true' action='POST'>
-			<BaseInput name='email' type='email' placeholder='email' />
-			<BaseInput name='name' type='text' placeholder='name' />
-			<BaseInput name='message' type='text' placeholder='message' />
+		// <BaseForm data-netlify='true' action='POST'>
+		// 	<BaseInput name='email' type='email' placeholder='email' />
+		// 	<BaseInput name='name' type='text' placeholder='name' />
+		// 	<BaseInput name='message' type='text' placeholder='message' />
+		// 	<div data-netlify-recaptcha='true' />
+		// 	<Button type='submit'>Submit</Button>
+		// </BaseForm>
+		<form name='contact' method='POST' data-netlify='true' action='/success'>
+			<p>
+				<label>
+					Your Name: <input type='text' name='name' />
+				</label>
+			</p>
+			<p>
+				<label>
+					Your Email: <input type='email' name='email' />
+				</label>
+			</p>
+			<p>
+				<label>
+					Message: <textarea name='message'></textarea>
+				</label>
+			</p>
+			<p>
+				<button type='submit'>Send</button>
+			</p>
 			<div data-netlify-recaptcha='true' />
-			<Button type='submit'>Submit</Button>
-		</BaseForm>
+		</form>
 	)
 }
