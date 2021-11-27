1b4d966a1822ff9da1ce1bde4c824f5d9eb836e4
Boyan Kmetov <boyankmetov@boyankmetovcomputer.home>
2021-11-27 17:50:31 +0100

    feat: scroll all pages to top on load

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+function ScrollToTop() {
+	const { pathname } = useLocation()
+
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [pathname])
+
+	return null
+}
+
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<ScrollToTop />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
