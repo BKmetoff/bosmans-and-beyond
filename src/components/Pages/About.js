3c61f76369eec8ceeacee318f4f02d6c317f30e0
Boyan Kmetov <boyankmetov@boyankmetovcomputer.home>
2021-12-02 16:31:25 +0100

    refactor: improve code style of about page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Theme } from '../theme/Theme'
-
-import ElisavetaImage from '../../assets/musicians/Elizaveta.jpg'
-import NataliaImage from '../../assets/musicians/Natalia.jpg'
-import DimitriImage from '../../assets/musicians/Dimitri.jpg'
 
 import { SectionWrapper, MotionWrapper } from '../backbone/Wrapper'
 import Sheet from '../backbone/Sheet'
 import { Text, Title } from '../backbone/Text'
 import { Divider } from '../backbone/Divider'
-
+import LinkToPersonalPage from '../backbone/Link/LinkToPersonalPage'
 import Image from '../backbone/Image'
+
+import { PEOPLE, mapNamesToBio } from '../../data/About/People'
 
 const PageWrapper = styled(SectionWrapper)`
 	margin-top: ${Theme.margin.XXL};
@@ -24,49 +21,44 @@ const TextWrapper = styled.div`
 	flex-direction: column;
 `
 
-function LinkToPersonalPage(url, name) {
-	return (
-		<Text externalLink>
-			<Link to={{ pathname: `${url}` }} target='_blank'>
-				{name.split(' ')[0]}'s website
-			</Link>
-		</Text>
-	)
-}
-
-function BioImage(index) {
+function BioImage(src) {
 	return (
 		<Image
 			round
 			shadow
 			width={Theme.bioImageSize}
 			height={Theme.bioImageSize}
-			src={
-				(index === 0 && ElisavetaImage) ||
-				(index === 1 && NataliaImage) ||
-				(index === 2 && DimitriImage)
-			}
+			src={src}
 		/>
 	)
 }
 
-export default function About({ people }) {
+function BioSection(person, index) {
+	const { fullName, photo, bio, website } = person
+	return (
+		<Sheet key={fullName} reversed={index === 1}>
+			{BioImage(photo)}
+			<TextWrapper>
+				<Text>{bio}</Text>
+				{LinkToPersonalPage(website, fullName)}
+			</TextWrapper>
+			<Divider />
+		</Sheet>
+	)
+}
+
+function NameMapper(people, mapNamesToBio) {
+	return Object.values(people).map((person, index) => {
+		return BioSection(mapNamesToBio[person], index)
+	})
+}
+
+export default function About() {
 	return (
 		<MotionWrapper>
 			<PageWrapper>
 				<Title>About us</Title>
-				{people.map((person, index) => {
-					return (
-						<Sheet key={index} reversed={index === 1}>
-							{BioImage(index)}
-							<TextWrapper>
-								<Text>{person.bio}</Text>
-								{LinkToPersonalPage(person.website, person.fullName)}
-							</TextWrapper>
-							{index + 1 !== people.length && <Divider />}
-						</Sheet>
-					)
-				})}
+				{NameMapper(PEOPLE, mapNamesToBio)}
 			</PageWrapper>
 		</MotionWrapper>
 	)
