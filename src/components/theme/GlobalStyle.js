5c2d7947757c83922c109bf65021397c3f8b2926
Boyan Kmetov <boyankmetov@boyankmetovcomputer.home>
2021-12-09 14:58:53 +0100

    style: extract GlobalStyle into separate file

---
+++ src/components/theme/GlobalStyle.js
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from 'styled-components'
+
+import OpenSans from '../../fonts/OpenSans.woff'
+import OpenSans2 from '../../fonts/OpenSans.woff2'
+
+import { COLORS } from './Theme'
+
+// <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div>
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Open Sans';
+    src: local('Open Sans'), local('OpenSans')
+    url(${OpenSans}), format('woff'),
+    url(${OpenSans2}), format('woff2');
+    font-weight: 400;
+  }
+  
+  body {
+  background: ${COLORS.background};
+  margin: 0;
+  font-family: 'Open Sans', sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  letter-spacing: 1px;
+  line-height: 1.9;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  }
+
+  *,
+  *::after,
+  *::before {
+    padding: 0;
+    margin: 0;
+    box-sizing: inherit;
+    /* overflow-x: hidden; */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+
+	textarea,
+	input,
+	::placeholder {
+		font-size:14px;
+		letter-spacing: 1px;
+		line-height: 1.9;
+		font-family: 'Open Sans', 'sans-serif', -apple-system, BlinkMacSystemFont,
+		'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+		'Droid Sans', 'Helvetica Neue', sans-serif;
+	}
+`
+
+export default GlobalStyle
